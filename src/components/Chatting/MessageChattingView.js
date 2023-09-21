705cda3d9d67d89de9cf8d64188c43ac2900317c
kwakseongjae <gkffhdnls13@gmail.com>
2023-09-21 23:58:28 +0900

    FIX: 채팅시 발생하는 오류 임시 해결

--- src/components/Chatting/MessageChattingView.js
+++ src/components/Chatting/MessageChattingView.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
-import { useRecoilValue } from "recoil";
+import { useRecoilValue, useRecoilState } from "recoil";
 import * as S from "../../styles/Chatting.styled";
 import { useParams } from "react-router";
 import {
   chattingStateByChattingId,
   chattingsState,
 } from "../../states/atoms/chattings";
+import dayjs from "dayjs";
+import { v4 as uuid } from "uuid";
 import TopBar from "./MessageTopBar";
 import MessageBox from "./MessageBox";
 import MessageInputBar from "./MessageInputBar";
@@ -15,8 +17,8 @@ function ChattingView() {
   const { roomId } = useParams(1);
   const chattings = useRecoilValue(chattingsState);
   const room = useRecoilValue(chattingStateByChattingId(parseInt(roomId)));
-
   let { userID } = useParams();
+
   const [isMe, setIsMe] = useState(true);
   const currentUser = isMe ? 1 : userID;
 
@@ -24,22 +26,28 @@ function ChattingView() {
     chattings[userID - 2],
   ]);
 
+  // const userChattingMessage = [...userChattingMessageSet[0].chatList];
+  // console.log(userChattingMessageSet);
+
   function switchUser() {
     setIsMe(!isMe);
   }
 
   function clickInputButton(messageUserInput) {
     const date = Date.now();
-    setUserChattingMessageSet((formerMessage) => [
-      ...formerMessage,
-      {
-        userId: currentUser,
-        content: messageUserInput,
-        date: date,
-        like: false,
-        chatId: "example1",
-      },
-    ]);
+    const newChat = {
+      userId: currentUser,
+      content: messageUserInput,
+      date: dayjs().format(),
+      like: false,
+      chatId: uuid(),
+    };
+    const tempObj = {
+      ...userChattingMessageSet[0],
+      chatList: [...userChattingMessageSet[0].chatList, newChat],
+    };
+
+    setUserChattingMessageSet([tempObj]);
   }
 
   return (
