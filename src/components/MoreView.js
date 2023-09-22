8a0634179e8260a6194f94e85e8b7debb713be7e
kwakseongjae <gkffhdnls13@gmail.com>
2023-09-22 16:32:48 +0900

    FIX: 더보기 오류 해결 - 기존 states/datas/User.js 파일 삭제로 인해 발생한 오류 수정

--- src/components/MoreView.js
+++ src/components/MoreView.js
@@ -1,16 +1,19 @@
 import React from "react";
 import * as S from "../styles/MoreView.styled";
-import userProfileSet from "../states/datas/User";
+import { useRecoilValue } from "recoil";
+import { userState } from "../states/atoms/user";
 
 function MoreView() {
+  const myProfile = useRecoilValue(userState);
+
   return (
     <S.Container>
       <h1>더보기</h1>
       <S.ProfileContainer>
         <S.ProfileName>
-          <h2>{userProfileSet[0].name}</h2>
+          <h2>{myProfile.userName}</h2>
           <S.ProfileImageNew
-            src={process.env.PUBLIC_URL + "/1.jpg"}
+            src={process.env.PUBLIC_URL + myProfile.userId + ".jpg"}
             alt=""
           ></S.ProfileImageNew>
         </S.ProfileName>
